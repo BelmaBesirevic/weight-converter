aa3bdc809c63fb6e38136c2f12e51829d382576d
Belma Besirevic <72704864+belmab@users.noreply.github.com>
2022-10-12 15:20:33 +0200

    gitignore

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+let pounds = document.getElementById('pounds'),
+  kilograms = document.getElementById('kilograms'),
+  grams = document.getElementById('grams'),
+  ounces = document.getElementById('ounces'),
+  weightInput = document.querySelector('.weight__inputs')
+
+weightInput.addEventListener('input', convertWeight)
+
+function convertWeight(e) {
+  if (e.target.classList.contains('pounds')) {
+    let x = e.target.value
+    kilograms.value = (x / 2.2046).toFixed(2)
+    grams.value = (x / 0.0022046).toFixed(2)
+    ounces.value = (x * 16).toFixed(2)
+  }
+  if (e.target.classList.contains('kilograms')) {
+    let x = e.target.value
+    pounds.value = (x * 2.2046).toFixed(2)
+    grams.value = (x * 1000).toFixed(2)
+    ounces.value = (x * 35.274).toFixed(2)
+  }
+  if (e.target.classList.contains('grams')) {
+    let x = e.target.value
+    kilograms.value = (x / 1000).toFixed(2)
+    pounds.value = (x * 0.0022046).toFixed(2)
+    ounces.value = (x * 0.035274).toFixed(2)
+  }
+
+  if (e.target.classList.contains('ounces')) {
+    let x = e.target.value
+    kilograms.value = (x / 35.274).toFixed(2)
+    grams.value = (x / 0.035274).toFixed(2)
+    pounds.value = (x * 0.0625).toFixed(2)
+  }
+}
